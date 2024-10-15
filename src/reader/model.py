e774c0f002175e0da4041434c11d4dd5886102ff
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-10-15 22:49:13 +0900

    chore: delete usaged module

--- src/reader/model.py
+++ src/reader/model.py
@@ -1,44 +1,158 @@
-import torch
+import logging
+import os
+
+from arguments import DataTrainingArguments
 from arguments import ModelArguments
+from data_processor import DataPostProcessor
+from data_processor import DataPreProcessor
+from datasets import DatasetDict
+from evaluate import load
+from trainer_qa import QuestionAnsweringTrainer
 from transformers import AutoConfig
 from transformers import AutoModelForQuestionAnswering
 from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+from transformers import TrainingArguments
+from utils_qa import check_no_error
+
+
+logger = logging.getLogger(__name__)
 
 
 class Reader:
-    # TODO: 리더 클래스 개발
-    def __init__(self, model_args: ModelArguments) -> None:
-        self.config = AutoConfig.from_pretrained(
-            model_args.config_name
-            if model_args.config_name is not None
-            else model_args.model_name_or_path,
-        )
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            model_args.tokenizer_name
-            if model_args.tokenizer_name is not None
-            else model_args.model_name_or_path,
-            use_fast=True,
-        )
-        self.model = AutoModelForQuestionAnswering.from_pretrained(
-            model_args.model_name_or_path,
-            from_tf=bool('.ckpt' in model_args.model_name_or_path),
-            config=self.config,
-        )
+    # TODO: 리더 클래스 개발, DataHandler 변경 사항에 맞게 개발
+    def __init__(
+        self,
+        model_args: ModelArguments,
+        data_args: DataTrainingArguments,
+        training_args: TrainingArguments,
+        datasets: DatasetDict,
+    ) -> None:
+        self.model_args = model_args
+        self.data_args = data_args
+        self.training_args = training_args
+        self.datasets = datasets
+
+        if self.training_args.do_train:
+            self.column_names = self.datasets['train'].column_names
+        else:
+            self.column_names = self.datasets['validation'].column_names
+
         self.output: dict = {}
 
-    def train(
-        self,
-        input_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        token_type_ids: torch.Tensor,
-    ) -> dict:
-        if self.model:
-            self.output = self.model(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
+    def load(self):
+        self.config = AutoConfig.from_pretrained(
+            self.model_args.config_name
+            if self.model_args.config_name is not None
+            else self.model_args.model_name_or_path,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_args.tokenizer_name
+            if self.model_args.tokenizer_name is not None
+            else self.model_args.model_name_or_path,
+            use_fast=True,
+        )
+        self.pad_on_right = self.tokenizer.padding_side == 'right'
+        self.last_checkpoint, self.max_seq_length = check_no_error(
+            data_args=self.data_args,
+            training_args=self.training_args,
+            datasets=self.datasets,
+            tokenizer=self.tokenizer,
+        )
+
+        self.model = AutoModelForQuestionAnswering.from_pretrained(
+            self.model_args.model_name_or_path,
+            from_tf=bool('.ckpt' in self.model_args.model_name_or_path),
+            config=self.config,
+        )
+
+    def train(self) -> None:
+        if self.training_args.do_train:
+            if 'train' not in self.datasets:
+                raise ValueError('--do_train requires a train dataset')
+            train_dataset = self.datasets['train']
+            train_dataset = train_dataset.map(
+                function=DataPreProcessor.process,
+                batched=True,
+                num_proc=self.data_args.preprocessing_num_workers,
+                remove_columns=self.column_names,
+                load_from_cache_file=not self.data_args.overwrite_cache,
             )
-        return self.output
+
+        if self.training_args.do_eval:
+            eval_dataset = self.datasets['validation']
+            eval_dataset = eval_dataset.map(
+                function=DataPreProcessor.process,
+                batched=True,
+                num_proc=self.data_args.preprocessing_num_workers,
+                remove_columns=self.column_names,
+                load_from_cache_file=not self.data_args.overwrite_cache,
+            )
+
+        data_collator = DataCollatorWithPadding(
+            tokenizer=self.tokenizer,
+            pad_to_multiple_of=8 if self.training_args.fp16 else None,
+        )
+
+        metric = load('squad')
+
+        def compute_metrics(p): return metric.compute(
+            predictions=p.predictions,
+            references=p.label_ids,
+        )
+
+        trainer = QuestionAnsweringTrainer(
+            model=self.model,
+            args=self.training_args,
+            train_dataset=train_dataset if self.training_args.do_train else None,
+            eval_dataset=eval_dataset if self.training_args.do_eval else None,
+            eval_examples=self.datasets['validation'] if self.training_args.do_eval else None,
+            tokenizer=self.tokenizer,
+            data_collator=data_collator,
+            post_process_function=DataPostProcessor.process,
+            compute_metrics=compute_metrics,
+        )
+
+        # Training
+        if self.training_args.do_train:
+            if self.last_checkpoint is not None:
+                checkpoint = self.last_checkpoint
+            elif os.path.isdir(self.model_args.model_name_or_path):
+                checkpoint = self.model_args.model_name_or_path
+            else:
+                checkpoint = None
+            train_result = trainer.train(resume_from_checkpoint=checkpoint)
+            trainer.save_model()  # Saves the tokenizer too for easy upload
+
+            metrics = train_result.metrics
+            metrics['train_samples'] = len(train_dataset)
+
+            trainer.log_metrics('train', metrics)
+            trainer.save_metrics('train', metrics)
+            trainer.save_state()
+
+            output_train_file = os.path.join(self.training_args.output_dir, 'train_results.txt')
+
+            with open(output_train_file, 'w') as writer:
+                logger.info('***** Train results *****')
+                for key, value in sorted(train_result.metrics.items()):
+                    logger.info(f'  {key} = {value}')
+                    writer.write(f'{key} = {value}\n')
+
+            # State 저장
+            trainer.state.save_to_json(
+                os.path.join(self.training_args.output_dir, 'trainer_state.json'),
+            )
+
+        # Evaluation
+        if self.training_args.do_eval:
+            logger.info('*** Evaluate ***')
+            metrics = trainer.evaluate()
+
+            metrics['eval_samples'] = len(eval_dataset)
+
+            trainer.log_metrics('eval', metrics)
+            trainer.save_metrics('eval', metrics)
 
     def predict(self, output: dict) -> dict:
         return {'predictions': output}
--- src/train.py
+++ src/train.py
@@ -1,49 +1,27 @@
 from arguments import DataTrainingArguments
 from arguments import ModelArguments
-from reader.data_handler import DataHandler
-from reader.evaluation import Evaluation
 from reader.model import Reader
 from reader.monitoring_tool import MonitoringTool
-from transformers import Trainer
+from transformers import HfArgumentParser
 from transformers import TrainingArguments
 
 
 def main():
     # TODO: 모델 학습 과정 추가 생성
-    model_args = ModelArguments()
-    data_args = DataTrainingArguments()
-    training_args = TrainingArguments(
-        output_dir='./results',
-        learning_rate=3e-5,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        num_train_epochs=3,
-        weight_decay=0.01,
+    parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, TrainingArguments),
     )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    reader_model = Reader(model_args)
-    reader_model.load_model()
-    reader_model.load_tokenizer()
+    reader_model = Reader(model_args, data_args, training_args)
+    reader_model.load()
 
-    data_handler = DataHandler(data_args)
-    evaluation = Evaluation()
+    # evaluation = Evaluation()
     monitor_tool = MonitoringTool()
-
-    train_dataset = data_handler.load_data()
-    eval_dataset = data_handler.load_data()
 
     monitor_tool.start_monitoring()
 
-    trainer = Trainer(
-        model=reader_model.model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        compute_metrics=evaluation.compute_metrics,
-    )
-
-    trainer.train()
-    trainer.evaluate()
+    reader_model.train()
 
     monitor_tool.stop_monitoring()
 
