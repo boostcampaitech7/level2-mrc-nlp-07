178ed8790f430c7d1b14ccc6fedd2fd4a860ba58
jduck301 <jduck301@gmail.com>
2024-10-24 03:07:39 +0000

    feat: test 데이터셋prediction
    - csv파일 형태 데이터셋 읽어 처리
    - prediction이 리스트로 들어와서 json.dump() 쓰도록 result_saver.py 수정

--- src/reader/model/result_saver.py
+++ src/reader/model/result_saver.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import json
 import os
 from logging import Logger
 from typing import Any
@@ -38,6 +39,14 @@ class ResultSaver:
 
     def save_predictions(self, predictions):
         output_file = os.path.join(self.training_args.output_dir, 'predictions.json')
+
+        # 변환할 딕셔너리 초기화
+        converted_dict = {}
+
+        # 원본 데이터를 변환
+        for item in predictions:
+            converted_dict[item['id']] = item['prediction_text']
+
         with open(output_file, 'w') as writer:
-            writer.write(predictions)
+            json.dump(predictions, writer, ensure_ascii=False, indent=4)
         self.logger.info(f'Predictions saved to {output_file}')
