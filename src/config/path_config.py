110cee6442a0eebffc12b783ff3270333c6e8bd0
Seungbeom oh <osr0912@gmail.com>
2024-10-28 10:37:52 +0900

    feat: add data_path

--- src/config/path_config.py
+++ src/config/path_config.py
@@ -4,10 +4,14 @@ from pathlib import Path
 BASE = Path(__file__).resolve().parent.parent.parent
 data_path = "./data/"
 wiki_path = "wikipedia_documents.json"
+train_data = "./data/train_dataset"
+test_data = "./data/test_dataset"
 train_name = "train.csv"
 test_name = "test.csv"
 
 DATA_PATH = Path(BASE, data_path)
 WIKI_PATH = Path(BASE, wiki_path)
+TRAIN_DATA_PATH = Path(BASE, train_data)
+TEST_DATA_PATH = Path(BASE, test_data)
 SAVE_TRAIN_PATH = Path(BASE, train_name)
-SAVE_TRAIN_PATH = Path(BASE, test_name)
+SAVE_TEST_PATH = Path(BASE, test_name)
