3c73c0ad8d0c5dd56eb6626467a108a4d2abe006
Seungbeom oh <osr0912@gmail.com>
2024-10-28 11:14:15 +0900

    chore: retriever

--- src/retriever/retriever.py
+++ src/retriever/retriever.py
@@ -1,6 +1,7 @@
 from datasets import concatenate_datasets, load_from_disk
 from src.retriever.retrieval.sparse_retrieval import SparseRetrieval
 from transformers import AutoTokenizer
+from src.config.path_config import TRAIN_DATA_PATH, TEST_DATA_PATH, SAVE_TRAIN_PATH, SAVE_TEST_PATH
 
 
 tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
@@ -15,14 +16,23 @@ retriever = SparseRetrieval(
 
 
 if __name__ == '__main__':
-    org_dataset = load_from_disk('./data/train_dataset')
-    print(org_dataset)
+    org_dataset = load_from_disk(TRAIN_DATA_PATH)
     full_ds = concatenate_datasets(
             [
                 org_dataset["train"].flatten_indices(),
                 org_dataset["validation"].flatten_indices(),
             ]
     )
-    print("*" * 40, "query dataset", "*" * 40)
+    print("*" * 40, "Train dataset", "*" * 40)
     print(full_ds)
+    test_dataset = load_from_disk(TEST_DATA_PATH)
+    test_dataset = test_dataset['validation'].flatten_indices()
+    print(test_dataset)
     retriever.get_sparse_embedding()
+    train_df = retriever.retrieve(full_ds, topk=10)
+    train_df.to_csv(SAVE_TRAIN_PATH)
+    print("*" * 40,'Save Train Result',"*" * 40)
+    retriever.get_score(train_df)
+    test_df = retriever.retrieve(test_dataset, topk=10)
+    test_df.to_csv(SAVE_TEST_PATH)
+    print("*" * 40,'Save Test Result',"*" * 40)
