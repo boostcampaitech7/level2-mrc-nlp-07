84f9b9275c3c3cad2b8f81ce593dc58cc416ec41
Seungbeom oh <osr0912@gmail.com>
2024-10-28 15:04:31 +0900

    chore: update config

--- src/config/path_config.py
+++ src/config/path_config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE = Path(__file__).resolve().parent.parent.parent
 data_path = "./data/"
-wiki_path = "filtered_wiki.json"
+wiki_path = "./data/filtered_wiki.json"
 train_data = "./data/train_dataset"
 test_data = "./data/test_dataset"
 train_name = "train.csv"
@@ -12,7 +12,10 @@ outputs = "./outputs"
 
 DATA_PATH = Path(BASE, data_path)
 WIKI_PATH = Path(BASE, wiki_path)
+
 TRAIN_DATA_PATH = Path(BASE, train_data)
 TEST_DATA_PATH = Path(BASE, test_data)
+
 SAVE_TRAIN_PATH = Path(BASE, train_name)
 SAVE_TEST_PATH = Path(BASE, test_name)
+OUTPUT_PATH = Path(BASE, outputs)
