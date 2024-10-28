1afc11743dc0e4530789708dbd4efbc0e8db5849
Seungbeom oh <osr0912@gmail.com>
2024-10-28 11:26:35 +0900

    chore: update path

--- src/config/path_config.py
+++ src/config/path_config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE = Path(__file__).resolve().parent.parent.parent
 data_path = "./data/"
-wiki_path = "wikipedia_documents.json"
+wiki_path = "filtered_wiki.json"
 train_data = "./data/train_dataset"
 test_data = "./data/test_dataset"
 train_name = "train.csv"
