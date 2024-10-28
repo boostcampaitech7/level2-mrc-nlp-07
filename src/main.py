1c75aa47f3788a8d02eeb037955cade018108081
Seungbeom oh <osr0912@gmail.com>
2024-10-28 14:04:59 +0900

    chore: delete unused files and update main.py

--- src/main.py
+++ src/main.py
@@ -5,9 +5,45 @@ from transformers import HfArgumentParser
 from transformers import TrainingArguments
 from utils.arguments import DataTrainingArguments
 from utils.arguments import ModelArguments
+from datasets import concatenate_datasets, load_from_disk
+from src.retriever.retrieval.sparse_retrieval import SparseRetrieval
+from transformers import AutoTokenizer
+from src.config.path_config import *
+
+def retriever():
+    tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+    retriever = SparseRetrieval(
+        tokenize_fn=tokenizer.tokenize,
+        data_path= DATA_PATH,
+        context_path= WIKI_PATH,
+        mode = "bm25",
+        max_feature=1000000,
+        ngram_range=(1,2),
+    )
+    org_dataset = load_from_disk(TRAIN_DATA_PATH)
+    full_ds = concatenate_datasets(
+            [
+                org_dataset["train"].flatten_indices(),
+                org_dataset["validation"].flatten_indices(),
+            ]
+    )
+    print("*" * 40, "Train dataset", "*" * 40)
+    print(full_ds)
+    test_dataset = load_from_disk(TEST_DATA_PATH)
+    test_dataset = test_dataset['validation'].flatten_indices()
+    print(test_dataset)
+    retriever.get_sparse_embedding()
+    train_df = retriever.retrieve(full_ds, topk=10)
+    train_df.to_csv(SAVE_TRAIN_PATH)
+    print("*" * 40,'Save Train Result',"*" * 40)
+    retriever.get_score(train_df)
+    test_df = retriever.retrieve(test_dataset, topk=10)
+    test_df.to_csv(SAVE_TEST_PATH)
+    print("*" * 40,'Save Test Result',"*" * 40)
+    print("*" * 40,'Finish Retriever',"*" * 40)
 
 
-def main():
+def reader():
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
@@ -19,4 +55,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    retriever()
+    reader()
