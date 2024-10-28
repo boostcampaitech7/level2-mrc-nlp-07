654e231650e64ef5e43f4b378a53db51bfbb9e5d
Seungbeom oh <osr0912@gmail.com>
2024-10-28 17:10:39 +0900

    chore: ununsed file and change file directory

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 
 import ast
-from src.reader.model.reader import Reader
+
+from reader.model.reader import Reader
 from transformers import HfArgumentParser
 from transformers import TrainingArguments
-from src.utils.arguments import DataTrainingArguments
-from src.utils.arguments import ModelArguments
+from utils.arguments import DataTrainingArguments
+from utils.arguments import ModelArguments
 from datasets import concatenate_datasets, load_from_disk
 from src.retriever.retrieval.sparse_retrieval import SparseRetrieval
 from transformers import AutoTokenizer
 from src.config.path_config import *
 from datasets import load_dataset, DatasetDict
 from src.config import key_names
-
 
 def retriever():
     tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
@@ -49,54 +49,58 @@ def retriever():
 
 def reader():
     # CSV 파일에서 데이터 로드
-    print("*" * 40,'Data Loading',"*" * 40)
-    train_retriever_dataset = load_dataset('csv', data_files=str(SAVE_TRAIN_PATH))
-    print(train_retriever_dataset)
-    
-    train_retriever_dataset[key_names.TRAIN] = train_retriever_dataset[key_names.TRAIN]\
-        .remove_columns(key_names.REMOVE_COLUMNS_FROM_RETRIEVER)\
-        .rename_column(key_names.RETRIEVAL_CONTEXT, key_names.CONTEXT)
+    train_retriever_dataset = load_dataset('csv', data_files=SAVE_TRAIN_PATH)
+    def preprocess(dataset):        
+        # 불필요한 컬럼 제거 및 'retrieval_context'를 'context'로 변경
+        train_retriever_dataset[key_names.TRAIN] = train_retriever_dataset[key_names.TRAIN]\
+            .remove_columns(key_names.REMOVE_COLUMNS_FROM_RETRIEVER)\
+            .rename_column(key_names.RETRIEVAL_CONTEXT, key_names.CONTEXT)
 
-    # DatasetDict로 train과 validation을 정의
-    train_dataset_dict = DatasetDict({
-        key_names.TRAIN: train_retriever_dataset[key_names.TRAIN],  # 'train' 스플릿으로 지정
-        key_names.VALIDATION: train_retriever_dataset[key_names.TRAIN]  # 'validation' 스플릿으로 지정
-    })
+        # 'answers' 필드를 파싱하여 딕셔너리로 변환하는 함수
+        def process_answers(example):
+            # 'answers' 필드가 문자열로 저장된 경우 이를 딕셔너리로 변환
+            if isinstance(example[key_names.ANSWER], str):
+                example[key_names.ANSWER] = ast.literal_eval(example[key_names.ANSWER])
+            return example
 
-    # 불필요한 컬럼 제거 및 'retrieval_context'를 'context'로 변경
-    
-    # 'answers' 필드를 파싱하여 딕셔너리로 변환하는 함수
-    def process_answers(example):
-        # 'answers' 필드가 문자열로 저장된 경우 이를 딕셔너리로 변환
-        if isinstance(example[key_names.ANSWERS], str):
-            example[key_names.ANSWERS] = ast.literal_eval(example[key_names.ANSWERS])
-        return example
+        # 'answers' 필드를 처리하여 파싱
+        train_retriever_dataset[key_names.TRAIN] = train_retriever_dataset[key_names.TRAIN]\
+        .map(process_answers)
 
-    # 'answers' 필드를 처리하여 파싱
-    train_dataset_dict[key_names.TRAIN] = train_dataset_dict[key_names.TRAIN].map(process_answers)
+        # DatasetDict로 train과 validation을 정의
+        train_dataset_dict = DatasetDict({
+            key_names.TRAIN: train_retriever_dataset[key_names.TRAIN],  # 'train' 스플릿으로 지정
+            key_names.VALIDATION: train_retriever_dataset[key_names.TRAIN]  # 'validation' 스플릿으로 지정
+        })
+        return train_retriever_dataset
 
     # Argument 설정
-    print("*" * 40,'Checking ModelArguments',"*" * 40)
     model_args = ModelArguments()
-    print("*" * 40,'Checking DataTrainingArguments',"*" * 40)
     data_args = DataTrainingArguments()
-    print("*" * 40,'Checking TrainingArugments',"*" * 40)
-    training_args = TrainingArguments(output_dir=OUTPUT_PATH)
-
+    training_args = TrainingArguments(output_dir=outputs)
+    
+    # output_dir은 training_args에 설정된 값을 사용
+    print('Output directory:', training_args.output_dir)
+    
     # 학습/평가/추론 설정
     training_args.do_train = True
-    training_args.do_eval = False
-    training_args.do_predict = False
-
+    training_args.do_eval = True
+    
     # 학습 하이퍼파라미터 설정
-    model_args.model_name_or_path = 'klue/bert-base'  # 학습에 사용할 모델
     training_args.learning_rate = 5e-5  # 학습률
     training_args.num_train_epochs = 1  # epoch 수
     training_args.per_device_train_batch_size = 16  # 학습 배치 사이즈
-
-    reader_model = Reader(model_args, data_args, training_args, train_dataset_dict)
+    reader_model = Reader(model_args, data_args, training_args, preprocess(train_retriever_dataset))
     reader_model.run()
 
+    #추론
+    predict_retriever_dataset = load_dataset('csv', data_files=SAVE_TEST_PATH)
+    training_args.do_train = False
+    training_args.do_eval = False
+    training_args.do_predict =True
+    model_args.model_name_or_path = outputs
+    reader_model = Reader(model_args, data_args, training_args, preprocess(predict_retriever_dataset))
+    reader_model.run()
 
 if __name__ == '__main__':
     retriever()
